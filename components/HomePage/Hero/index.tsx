36e1b14d032bfde23480c8132a78a366930b9578
Kolby Sisk <kolbysisk@Kolbys-MacBook-Pro.local>
2019-10-30 10:48:09 -0400

    Moved images into public dir

--- components/HomePage/Hero/index.tsx
+++ components/HomePage/Hero/index.tsx
@@ -7,7 +7,7 @@ const HeroComponent = () => {
         <HeroStyles.Header>
           <HeroStyles.Logo
             dangerouslySetInnerHTML={{
-              __html: require('../../../images/logo-white.svg?include&size=80'),
+              __html: require('../../../public/images/logo-white.svg?include&size=80'),
             }}
           />
         </HeroStyles.Header>
@@ -19,9 +19,15 @@ const HeroComponent = () => {
         </HeroStyles.ContentContainer>
       </HeroStyles.AbsoluteContainer>
       <HeroStyles.Background>
-        <source srcSet={require('../../../images/hero-bg.jpg?webp&size=1400')} type="image/webp" />
-        <source srcSet={require('../../../images/hero-bg.jpg?size=1400')} type="image/jpeg" />
-        <img src={require('../../../images/hero-bg.jpg?size=1400')} />
+        <source
+          srcSet={require('../../../public/images/hero-bg.jpg?webp&size=1400')}
+          type="image/webp"
+        />
+        <source
+          srcSet={require('../../../public/images/hero-bg.jpg?size=1400')}
+          type="image/jpeg"
+        />
+        <img src={require('../../../public/images/hero-bg.jpg?size=1400')} />
       </HeroStyles.Background>
     </HeroStyles.Root>
   );
--- components/HomePage/home-page.styles.tsx
+++ components/HomePage/home-page.styles.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-const bgImage = require('../../images/bg.jpg?webp&size=1400');
+const bgImage = require('../../public/images/bg.jpg?webp&size=1400');
 
 export const Root = styled.div({
   height: '100%',
