f6d29911e73090054a01d249c2c69ec652ff0905
Kolby <kolby.sisk@ncr.com>
2019-11-13 18:58:53 -0500

    slack verify

---
+++ pages/api/verify.tsx
@@ -0,0 +1,19 @@
+import signVerification from '../../server/slack-verify';
+
+export default (req, res) => {
+  const { method } = req;
+
+  console.log(req);
+
+  if (method === 'POST') {
+    if (!signVerification(req, res)) return;
+    res.send(req.body.challenge);
+  }
+
+  // method isn't allowed so return 405
+  else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${method} Not Allowed`);
+    return;
+  }
+};
