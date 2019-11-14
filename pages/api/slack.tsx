e4de1464d7efc4e2076f0e6593b465fa49f271f3
Kolby <kolby.sisk@ncr.com>
2019-11-13 19:36:13 -0500

    slack verify

--- pages/api/slack.tsx
+++ pages/api/slack.tsx
@@ -5,24 +5,7 @@ export default (req, res) => {
 
   if (method === 'POST') {
     if (!signVerification(req, res)) return;
-    res.send(200);
-
-    if (req.body.event.bot_id) return;
-
-    const slackMessage: string = req.body.event.text;
-    const slackMessageSplit: string[] = slackMessage.split('--');
-
-    if (slackMessageSplit.length) {
-      const g: any = global;
-      const io = g._io;
-
-      const socketId = slackMessageSplit[0].trim();
-      const message = slackMessageSplit[1].trim();
-
-      if (io.sockets.connected[socketId]) {
-        io.sockets.connected[socketId].emit('message', message);
-      }
-    }
+    res.send(req.body.challenge);
   }
 
   // method isn't allowed so return 405
