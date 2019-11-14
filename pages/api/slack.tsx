6498b9e9d2b345655e2a7c9ea4e39c2f5cc5359b
Kolby <kolby.sisk@ncr.com>
2019-11-14 11:42:59 -0500

    slack event endpoint

--- pages/api/slack.tsx
+++ pages/api/slack.tsx
@@ -1,42 +1,3 @@
-import signVerification from '../../server/slack-verify';
-
-export default (req, res) => {
-  const { method } = req;
-
-  if (method === 'POST') {
-    if (!signVerification(req, res)) return;
-    res.send(200);
-
-    if (req.body.event.bot_id) return;
-
-    const slackMessage: string = req.body.event.text;
-    const slackMessageSplit: string[] = slackMessage.split('--');
-
-    if (slackMessageSplit.length) {
-      const g: any = global;
-      const io = g._io;
-
-      const socketId = slackMessageSplit[0].trim();
-      const message = slackMessageSplit[1].trim();
-
-      if (io.sockets.connected[socketId]) {
-        io.sockets.connected[socketId].emit('message', message);
-      }
-    }
-  }
-
-  // method isn't allowed so return 405
-  else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${method} Not Allowed`);
-    return;
-  }
-};
-
-/**
- * Uncomment for slack even verification
- */
-
 // import signVerification from '../../server/slack-verify';
 
 // export default (req, res) => {
@@ -44,7 +5,24 @@ export default (req, res) => {
 
 //   if (method === 'POST') {
 //     if (!signVerification(req, res)) return;
-//     res.send(req.body.challenge);
+//     res.send(200);
+
+//     if (req.body.event.bot_id) return;
+
+//     const slackMessage: string = req.body.event.text;
+//     const slackMessageSplit: string[] = slackMessage.split('--');
+
+//     if (slackMessageSplit.length) {
+//       const g: any = global;
+//       const io = g._io;
+
+//       const socketId = slackMessageSplit[0].trim();
+//       const message = slackMessageSplit[1].trim();
+
+//       if (io.sockets.connected[socketId]) {
+//         io.sockets.connected[socketId].emit('message', message);
+//       }
+//     }
 //   }
 
 //   // method isn't allowed so return 405
@@ -54,3 +32,25 @@ export default (req, res) => {
 //     return;
 //   }
 // };
+
+/**
+ * Uncomment for slack even verification
+ */
+
+import signVerification from '../../server/slack-verify';
+
+export default (req, res) => {
+  const { method } = req;
+
+  if (method === 'POST') {
+    if (!signVerification(req, res)) return;
+    res.send(req.body.challenge);
+  }
+
+  // method isn't allowed so return 405
+  else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${method} Not Allowed`);
+    return;
+  }
+};
